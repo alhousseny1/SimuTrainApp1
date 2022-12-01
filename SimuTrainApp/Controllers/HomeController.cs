313ee0d1a4973f3b37aada79808ff309e332ae38
alhousseny1 <senydiaby93@gmail.com>
2022-12-01 16:57:51 +0100

    model added

--- SimuTrainApp/Controllers/HomeController.cs
+++ SimuTrainApp/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace SimuTrainApp.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ SimuTrainApp/Models/ViewModels/Home/Gare.cs
@@ -0,0 +1,26 @@
+﻿namespace SimuTrainApp.Models.ViewModels.Home
+{
+    public class Gares
+    {
+        #region "Properties gare"
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public int NBParcels { get; set; }
+        public int NBPassenger { get; set; }
+        #endregion
+
+        #region "Method gare"
+        public Gares(
+            string Name,
+            string Place,
+            int NBParcels,
+            int NBPassenger)
+        {
+            this.Name = Name;
+            this.Place = Place;
+            this.NBParcels = NBParcels;
+            this.NBPassenger = NBPassenger;
+        }
+        #endregion
+    }
+}
