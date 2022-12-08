bc90c0e849835ee2c47558e36db64a5771499027
lavyfute <ibombo.borel@outlook.fr>
2022-12-08 14:24:27 +0100

    feat[ SimTrain1 ] : Creation of our models
    Creation of our classes: Train, Road and Train with their properties, builders and methods

---
+++ SimuTrainApp/Models/Station.cs
@@ -0,0 +1,35 @@
+﻿namespace SimuTrainApp.Models;
+
+public class Station
+{
+    #region "Properties Station"
+    public string Name { get; set; }
+    public string Place { get; set; }
+    public int NBParcels { get; set; }
+    public int NBPassenger { get; set; }
+    public int IdTrainInStation { get; set; }
+
+    #endregion
+
+    #region "Constructor STation"
+    public Station()
+    {
+
+    }
+    public Station(
+        string Name,
+        string Place,
+        int NBParcels,
+        int NBPassenger)
+    {
+        this.Name = Name;
+        this.Place = Place;
+        this.NBParcels = NBParcels;
+        this.NBPassenger = NBPassenger;
+    }
+    #endregion
+
+    #region "Methods"
+
+    #endregion
+}
