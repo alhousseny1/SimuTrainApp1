7bb8e5802f64e0b3feda295215434e33e25831f7
alhousseny1 <senydiaby93@gmail.com>
2022-12-09 14:19:10 +0100

    style added

---
+++ SimuTrainApp/Models/Route.cs
@@ -0,0 +1,35 @@
+﻿namespace SimuTrainApp.Models
+{
+    public class RouteOfTrain
+    {
+
+
+        #region "Properties Route"
+
+        public Station DepartureStation { get; set; }
+        public string DepartureTime { get; set; }
+        public string ArrivalTime { get; set; }
+        public Station ArrivalStation { get; set; }
+
+        #endregion
+
+        #region "Constructor Route"
+
+        public RouteOfTrain()
+        {
+
+        }
+
+        public RouteOfTrain(Station IdDepartureStation, String departureTime, string arrivalTime, Station IdArrivalStation)
+        {
+            this.DepartureStation = IdDepartureStation;
+            DepartureTime = departureTime;
+            ArrivalTime = arrivalTime;
+            this.ArrivalStation = IdArrivalStation;
+        }
+
+
+        #endregion
+
+    }
+}
