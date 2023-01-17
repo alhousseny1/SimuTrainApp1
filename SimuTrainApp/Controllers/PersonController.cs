3ef665ecce6009be488508e9adb066fdbfa3b7ae
lavyfute <ibombo.borel@outlook.fr>
2023-01-17 15:20:49 +0100

    feat [SimTrain7] : Generating PersonController with Entity Framework
    Generating of PersonController and all views

--- SimuTrainApp/Controllers/PersonController.cs
+++ SimuTrainApp/Controllers/PersonController.cs
@@ -19,13 +19,13 @@ namespace SimuTrainApp.Controllers
             _context = context;
         }
 
-        // GET: Person
+        // GET: People
         public async Task<IActionResult> Index()
         {
               return View(await _context.Person.ToListAsync());
         }
 
-        // GET: Person/Details/5
+        // GET: People/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Person == null)
@@ -34,7 +34,7 @@ namespace SimuTrainApp.Controllers
             }
 
             var person = await _context.Person
-                .FirstOrDefaultAsync(m => m.ID == id);
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (person == null)
             {
                 return NotFound();
@@ -43,18 +43,18 @@ namespace SimuTrainApp.Controllers
             return View(person);
         }
 
-        // GET: Person/Create
+        // GET: People/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Person/Create
+        // POST: People/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name")] Person person)
+        public async Task<IActionResult> Create([Bind("Id,Name")] Person person)
         {
             if (ModelState.IsValid)
             {
@@ -65,7 +65,7 @@ namespace SimuTrainApp.Controllers
             return View(person);
         }
 
-        // GET: Person/Edit/5
+        // GET: People/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Person == null)
@@ -81,14 +81,14 @@ namespace SimuTrainApp.Controllers
             return View(person);
         }
 
-        // POST: Person/Edit/5
+        // POST: People/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int? id, [Bind("ID,Name")] Person person)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Person person)
         {
-            if (id != person.ID)
+            if (id != person.Id)
             {
                 return NotFound();
             }
@@ -102,7 +102,7 @@ namespace SimuTrainApp.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!PersonExists(person.ID))
+                    if (!PersonExists(person.Id))
                     {
                         return NotFound();
                     }
@@ -116,7 +116,7 @@ namespace SimuTrainApp.Controllers
             return View(person);
         }
 
-        // GET: Person/Delete/5
+        // GET: People/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Person == null)
@@ -125,7 +125,7 @@ namespace SimuTrainApp.Controllers
             }
 
             var person = await _context.Person
-                .FirstOrDefaultAsync(m => m.ID == id);
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (person == null)
             {
                 return NotFound();
@@ -134,10 +134,10 @@ namespace SimuTrainApp.Controllers
             return View(person);
         }
 
-        // POST: Person/Delete/5
+        // POST: People/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int? id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
             if (_context.Person == null)
             {
@@ -153,9 +153,9 @@ namespace SimuTrainApp.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool PersonExists(int? id)
+        private bool PersonExists(int id)
         {
-          return _context.Person.Any(e => e.ID == id);
+          return _context.Person.Any(e => e.Id == id);
         }
     }
 }
