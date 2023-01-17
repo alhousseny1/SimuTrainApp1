f23ebb958205c1bd5a9a74ca3d605bf25ac2c625
lavyfute <ibombo.borel@outlook.fr>
2023-01-17 15:15:46 +0100

    fix : Addind Accueil method in HomeController
    the app could not find "Accueil" view

--- SimuTrainApp/Controllers/HomeController.cs
+++ SimuTrainApp/Controllers/HomeController.cs
@@ -15,6 +15,12 @@ namespace SimuTrainApp.Controllers
 
         public IActionResult Index()
         {
+
+            return View();
+        }
+        public IActionResult Accueil()
+        {
+
             return View();
         }
 
