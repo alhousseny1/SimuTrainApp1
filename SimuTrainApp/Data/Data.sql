33e8df386e8c948fc2d1e8aef22ed175735813d7
lavyfute <ibombo.borel@outlook.fr>
2023-01-17 15:26:12 +0100

    feat [SimTrain7] : Add migration and Create Db

---
+++ SimuTrainApp/Data/Data.sql
@@ -0,0 +1,52 @@
+﻿DBCC CHECKIDENT([Station], RESEED, 0)
+DBCC CHECKIDENT(RouteOfTrain, RESEED, 0)
+DBCC CHECKIDENT([Train], RESEED, 0)
+
+
+SET IDENTITY_INSERT [dbo].[RouteOfTrain] ON
+INSERT INTO [dbo].[RouteOfTrain] ([Id], [NbRoute]) VALUES (1, 1)
+INSERT INTO [dbo].[RouteOfTrain] ([Id], [NbRoute]) VALUES (2, 3)
+INSERT INTO [dbo].[RouteOfTrain] ([Id], [NbRoute]) VALUES (3, 6)
+INSERT INTO [dbo].[RouteOfTrain] ([Id], [NbRoute]) VALUES (4, 14)
+INSERT INTO [dbo].[RouteOfTrain] ([Id], [NbRoute]) VALUES (5, 4)
+SET IDENTITY_INSERT [dbo].[RouteOfTrain] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Station] ON
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (1, N'Mairie de Saint-Ouen', N'7 pl Rpublique, 93400 Saint Ouen', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (2, N'Saint-Ouen', N'Rue du Docteur Bauer, 93400 Saint-Ouen-sur-Seine', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (3, N'Porte de Clichy', N'3 BD Berthier, 75017 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (4, N'Pont Cardinet', N'Rue Jouffroy d''Abbans, 75017 17me Arrondissement
+', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (5, N'Saint-Lazare', N'Rue Saint-Lazare, 75009 9me Arrondissement ', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (6, N'Madeleine', N'11 Rue du Chevalier de Saint-George, 75008 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (7, N'Pyramides', N'Saint-Honor, Rue des Pyramides, 75001 1er Arrondissement', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (8, N'Châtelet', N'104 Rue de Rivoli, 75001 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (9, N'Gare de Lyon', N'Place Louis Armand, 75012 Paris
+', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (10, N'Cour Saint-Émilion', N'Parc de Bercy, 75012 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (11, N'Bibliothèque François-Mitterrand ', N'Tolbiac Street, 75013 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (12, N'Olympiades', N'91 r Tolbiac, 75013 Paris', 2)
+SET IDENTITY_INSERT [dbo].[Station] OFF
+SET IDENTITY_INSERT [dbo].[Station] ON
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (1, N'Mairie de Saint-Ouen', N'7 pl Rpublique, 93400 Saint Ouen', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (2, N'Saint-Ouen', N'Rue du Docteur Bauer, 93400 Saint-Ouen-sur-Seine', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (3, N'Porte de Clichy', N'3 BD Berthier, 75017 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (4, N'Pont Cardinet', N'Rue Jouffroy d''Abbans, 75017 17me Arrondissement
+', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (5, N'Saint-Lazare', N'Rue Saint-Lazare, 75009 9me Arrondissement ', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (6, N'Madeleine', N'11 Rue du Chevalier de Saint-George, 75008 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (7, N'Pyramides', N'Saint-Honor, Rue des Pyramides, 75001 1er Arrondissement', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (8, N'Châtelet', N'104 Rue de Rivoli, 75001 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (9, N'Gare de Lyon', N'Place Louis Armand, 75012 Paris
+', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (10, N'Cour Saint-Émilion', N'Parc de Bercy, 75012 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (11, N'Bibliothèque François-Mitterrand ', N'Tolbiac Street, 75013 Paris', 2)
+INSERT INTO [dbo].[Station] ([Id], [Name], [Place], IdRoute) VALUES (12, N'Olympiades', N'91 r Tolbiac, 75013 Paris', 2)
+SET IDENTITY_INSERT [dbo].[Station] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Train] ON
+INSERT INTO [dbo].[Train] ([Id], [Matricule], [Capacity], [Color], [Speed], [IdRoute], [StationId]) VALUES (1, N'TX023', 150, N'Blue', 200, 4, 1)
+INSERT INTO [dbo].[Train] ([Id], [Matricule], [Capacity], [Color], [Speed], [IdRoute], [StationId]) VALUES (2, N'TX123', 100, N'White', 150, 4, 2)
+SET IDENTITY_INSERT [dbo].[Train] OFF
---
+++ SimuTrainApp/Migrations/20230117095021_CreateDb.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimuTrainApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RouteOfTrain",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NbRoute = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteOfTrain", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Station",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IdRoute = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Station", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Station_RouteOfTrain_IdRoute",
+                        column: x => x.IdRoute,
+                        principalTable: "RouteOfTrain",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Train",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Matricule = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    IdRoute = table.Column<int>(type: "int", nullable: false),
+                    StationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Train", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Train_RouteOfTrain_IdRoute",
+                        column: x => x.IdRoute,
+                        principalTable: "RouteOfTrain",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Train_Station_StationId",
+                        column: x => x.StationId,
+                        principalTable: "Station",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parcel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    IdTrain = table.Column<int>(type: "int", nullable: false),
+                    IdStation = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parcel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parcel_Station_IdStation",
+                        column: x => x.IdStation,
+                        principalTable: "Station",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Parcel_Train_IdTrain",
+                        column: x => x.IdTrain,
+                        principalTable: "Train",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdStation = table.Column<int>(type: "int", nullable: true),
+                    IdTrain = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Person_Station_IdStation",
+                        column: x => x.IdStation,
+                        principalTable: "Station",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Person_Train_IdTrain",
+                        column: x => x.IdTrain,
+                        principalTable: "Train",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parcel_IdStation",
+                table: "Parcel",
+                column: "IdStation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parcel_IdTrain",
+                table: "Parcel",
+                column: "IdTrain");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_IdStation",
+                table: "Person",
+                column: "IdStation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_IdTrain",
+                table: "Person",
+                column: "IdTrain");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Station_IdRoute",
+                table: "Station",
+                column: "IdRoute");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Train_IdRoute",
+                table: "Train",
+                column: "IdRoute");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Train_StationId",
+                table: "Train",
+                column: "StationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parcel");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Train");
+
+            migrationBuilder.DropTable(
+                name: "Station");
+
+            migrationBuilder.DropTable(
+                name: "RouteOfTrain");
+        }
+    }
+}
